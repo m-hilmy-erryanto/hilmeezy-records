5ce3e287d2eb6d53ed23484e19d4a3c19b64a562
m-hilmy-erryanto <muhammad.hilmy22@ui.ac.id>
2023-09-11 12:05:19 +0700

    fourth commit

--- main/views.py
+++ main/views.py
@@ -3,11 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name': 'To Pimp a Butterfly',
-        'amount': '1',
-        'description': 'Kendrick Lamar',
-        'category': 'Rap',
-        'price': '$14'
+        'Application Name': 'hilmeezy records',
+        'Name': 'Muhammad Hilmy Erryanto',
+        'Class': 'PBP F',
     }
 
     return render(request, "main.html", context)
